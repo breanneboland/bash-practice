a45f4cfd08a13ce34ecff1999eb44ca0edd2928d
Breanne Boland <breanneboland@gmail.com>
2015-12-30 14:45:13 -0800

    in which I got it to select a random word from a list, yesssss

--- random-word.sh
+++ random-word.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-WordArray=( `cat "short-word-list.txt"` )
+WordArray=( `cat "shorter-words.txt"` )
 
 # echo ${WordArray[@]}
 
-for word in "${WordArray[@]}"
-do
-	echo $word
-done
+# for word in "${WordArray[@]}"
+# do
+# 	echo $word
+# done
 
-echo "Those are the words, yay!"
+# echo "Those are the words, yay!"
+
+# WordIndex=$(( ( RANDOM % 10 )  + 1 ))
+
+ListLength=${#WordArray[@]}
+# echo "List length: $ListLength"
+RandomIndex=$(( ( RANDOM % ListLength )  + 1 ))
+echo ${WordArray[RandomIndex]}
