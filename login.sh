d9d330bb9cefcc6f1b0db5b28e432557da00d440
Breanne Boland <breanneboland@gmail.com>
2016-02-11 11:36:27 -0800

    moving along in the tutorial

---
+++ login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p 'Username: ' uservar
+read -sp 'Password: ' passvar
+echo
+echo Thank you, $uservar. We now have your login details. 
+sleep 3
+echo And will shortly use them... for EVIL.
--- substring-test.sh
+++ substring-test.sh
@@ -1,9 +1,24 @@
-LIST="This is a longer string that I will be searching inside of"
-SOURCE="string"
+# FILE=./phone-file.txt
+# SUBSTRING="http"
 
-if echo "$LIST" | grep -q "$SOURCE"; then
-	echo "matched";
-	
-else
-	echo "no match";
-fi 
+# echo "$FILE"
+
+# if echo "$FILE" | grep -q "$SUBSTRING"; then
+# 	echo "matched";
+# else
+# 	echo "no match";
+# fi 
+
+PATTERN=$1
+FILE=$2
+
+for line in $FILE: 
+if grep -q $PATTERN $line;
+	 then
+	     echo "Here are the Strings with the Pattern '$PATTERN':"
+	     echo -e "$(grep $PATTERN $FILE)\n" >> cubby-file.txt
+	 else
+	     echo "Error: The Pattern '$PATTERN' was NOT Found in '$FILE'"
+	     echo "Exiting..."
+	     exit 0
+fi
